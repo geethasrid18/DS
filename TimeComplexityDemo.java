a387a28bd74c2874d7f99b867973cda2aca098f9
geethasrid18 <geethasrid1803@gmail.com>
2024-03-14 22:57:57 +0530

    commit

--- TimeComplexityDemo.java
+++ TimeComplexityDemo.java
@@ -8,22 +8,24 @@ public class TimeComplexityDemo {
     System.out.println(tc.findSum(9999));
     System.out.println("Time Taken-"+(System.currentTimeMillis()-now)+"ms");
  }
-// public int findSum(int s)
-// {
-//     return s*(s+1)/2;
-// }
+public int findSum(int s)
+{
+    return s*(s+1)/2;
+}
 //Output:
 //49995000
 //Time Taken-4.0ms
-public int findSum(int s)
-{
-    int sum=0;
-    for(int i=1;i<=s;i++)
-    {
-sum=sum+i;
-    }
-    return sum;
-}
+
+// public int findSum(int s)
+// {
+//     int sum=0;
+//     for(int i=1;i<=s;i++)
+//     {
+// sum=sum+i;
+//     }
+//     return sum;
+// }
+
 //Output:
 //49995000
 //Time Taken-14.0ms  
