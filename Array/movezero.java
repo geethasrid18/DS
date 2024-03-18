8ab925a18e7905482b700f9994e568c255e96d89
geethasrid18 <geethasrid1803@gmail.com>
2024-03-18 19:02:47 +0530

    commit

---
+++ Array/movezero.java
@@ -0,0 +1,34 @@
+//Move all the zero in the array to the end
+package Array;
+import java.util.*;
+public class movezero {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        zero(arr);
+    }
+    static void zero(int arr[])
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[i]==0 ) 
+                {
+                    int temp= arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+    }
+}
+}
